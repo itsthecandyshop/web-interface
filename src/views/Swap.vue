c2a812eff25a340912dfb2249f3358fa9e3c16b1
Arth Patel <arth@matic.network>
2020-05-09 16:03:58 +0530

    init

---
+++ src/views/Swap.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <form>
+      <div>
+        <label for="input">Input</label>
+        <input type="number" name="input" />
+        <select>
+          <option v-for="curr in currencyList" :key="curr.id" :value="curr.id">
+            {{ curr.symbol }}
+          </option>
+        </select>
+      </div>
+      <div>
+        <label for="output">Output</label>
+        <input type="number" name="output" />
+        <select>
+          <option v-for="curr in currencyList" :key="curr.id" :value="curr.id">
+            {{ curr.symbol }}
+          </option>
+        </select>
+      </div>
+      <div>
+        <button type="submit" @click.prevent="handleExchange">
+          Exchange
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component({
+  name: "Swap"
+})
+export default class HelloWorld extends Vue {
+  currencyList = [
+    {
+      id: "1",
+      name: "Ether",
+      symbol: "ETH"
+    },
+    {
+      id: "2",
+      name: "Dai Stablecoin",
+      symbol: "DAI"
+    }
+  ];
+
+  handleExchange() {}
+}
+</script>
